c309a7dca4fa2d8cdd48c3b2e21108e2476594bf
Prasad <prasadhiwarkar18@gmail.com>
2023-01-29 15:39:28 +0530

    Modified IV1\app\src\main\java\com\example\iv1\data\Drug.kt to obtain data from firebase in the form of HashMap. ------------------------------------------------------------ Converted compatible_list parameter from ArrayList<CompatibleList> to HashMap<String, Any>. ------------------------------------------------------------ Converted iv_fluid parameter from ArrayList<IVFluid> to HashMap<String, Any>.

--- app/src/main/java/com/example/iv1/data/Drug.kt
+++ app/src/main/java/com/example/iv1/data/Drug.kt
@@ -1,13 +1,11 @@
 package com.example.iv1.data
 
-import java.lang.reflect.Constructor
-
 data class Drug (
-    var compatible_drugs: ArrayList<CompatibleDrugs> = ArrayList(),
+    var compatible_drugs: HashMap<String, Any> = HashMap(),
     var drug_name: String = "",
     var id: Int = 0,
     var incompatible_drugs: ArrayList<String> = ArrayList(),
-    var iv_fluid: ArrayList<IVFluid> = ArrayList(),
-    var pH: Double = 0.0,
+    var iv_fluid: HashMap<String, Any> = HashMap(),
+    var pH: String = "",
     var storage: String = ""
 )
