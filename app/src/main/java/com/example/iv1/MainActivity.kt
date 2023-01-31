708fcbc39726e9883114cb37442c837cd0fd2eb8
Prasad <prasadhiwarkar18@gmail.com>
2023-01-31 12:20:44 +0530

    Set StartScreen.kt as an entry into the application.

--- app/src/main/java/com/example/iv1/MainActivity.kt
+++ app/src/main/java/com/example/iv1/MainActivity.kt
@@ -3,17 +3,13 @@ package com.example.iv1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import com.example.iv1.ui.DrugViewModel
-import com.example.iv1.ui.SetData
 import com.example.iv1.ui.theme.IV1Theme
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: DrugViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +18,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    SetData(viewModel = viewModel)
+                    Start()
                 }
             }
         }
